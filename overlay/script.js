b9be5c9064bbea66bd4c04e6759b015471023cc0
abtaskbot <ARPITABHOWMICK120@GMAIL.COM>
2026-01-02 14:56:07 +0530

    Create script.js

---
+++ overlay/script.js
@@ -0,0 +1,24 @@
+const SCROLL_SPEED = 25; 
+// 👆 change later: higher = slower scroll, lower = faster
+
+const ws = new WebSocket(
+  location.origin.replace("http", "ws")
+);
+
+ws.onmessage = e => {
+  const data = JSON.parse(e.data);
+
+  const container = document.getElementById("task-container");
+  container.innerHTML = "";
+
+  Object.entries(data.tasks).forEach(([user, info]) => {
+    const div = document.createElement("div");
+    div.className = "task";
+    div.style.animationDuration = `${SCROLL_SPEED}s`;
+    div.innerText = `${user}: ${info.task} ${info.done ? "✔" : ""}`;
+    container.appendChild(div);
+  });
+
+  document.getElementById("counter").innerText =
+    `${data.completedCount} / ${Object.keys(data.tasks).length}`;
+};
